c0bdd6c793a736e4f51da25cc49942fff7a9bce8
Geoff Sargison <gsarge.inzid@gmail.com>
2018-10-24 11:04:05 +1300

    Added new js-sandbox files

--- projects/js-sandbox/js/slideshow-app.js
+++ projects/js-sandbox/js/slideshow-app.js
@@ -5,18 +5,34 @@ let slides = ["Extremely old", "Retired and broke", "A general drain on society"
 
 let bgColour = ["green", "orange", "blue", "teal", "pink", "lightgreen"];
 
+let intervalCheck = 1;
+
 function displaySlide(slide, slideColour) {
-    setInterval(function () {
         slideShowEl.innerHTML = slide;
-        document.body.style.transition = "all 2s";
+        // document.body.style.transition = "all 2s";
         document.body.style.backgroundColor = slideColour;
         slide = slides[Math.floor(Math.random() * slides.length)];
         slideColour = bgColour[Math.floor(Math.random() * bgColour.length)];
-    }, 3000)
 }
 
 let slide = slides[Math.floor(Math.random() * slides.length)];
 let slideColour = bgColour[Math.floor(Math.random() * bgColour.length)];
+
+slideShowEl.addEventListener('click', function (){
+    if (intervalCheck == 1) {
+        intervalCheck = intervalCheck - 1;    
+        clearInterval(displaySlide);
+        setInterval(function () {
+            displaySlide(slide, slideColour);
+        }, 200);
+    } else if (intervalCheck == 0) {
+        intervalCheck = intervalCheck + 1;
+        clearInterval(displaySlide);
+        setInterval(function () {
+            displaySlide(slide, slideColour);
+        }, 3000);
+    }
+});
 
 displaySlide(slide, slideColour);
 
