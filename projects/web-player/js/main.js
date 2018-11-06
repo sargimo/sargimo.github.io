b948ee7abe99f543c897c2dc2c35580ddd3c6a62
Geoff Sargison <gsarge.inzid@gmail.com>
2018-11-06 18:54:08 +1300

    Updated web player to include category list

--- projects/web-player/js/main.js
+++ projects/web-player/js/main.js
@@ -3,91 +3,35 @@ let videoTitleEl = $('#videoTitle');
 let videoDescEl = $('#videoDesc');
 let videoSearchEl = $('#videoSearch');
 let videoThumbsEl = $('#hexGrid');
+let categoryListEl = $('.category-list')
 let containerEl = $('.container');
+let backToVideosBtn = $('#backToVideos');
 // let thumbHtml ="";
 
-let videoList = {
-    "videos": [
-        {
-            "name": "Taylor Swift the GOAT",
-            "source": "-aLYvZ5sX28",
-            "desc": "Best goat remix you ever fkn seen.",
-            "category": "Animal Remix",
-            "thumbnail": "images/goat1.jpg"
-        },
-        {
-            "name": "Water Pump Blues",
-            "source": "o5ZEYMl9VIs",
-            "desc": "Jammin' with a water pump.",
-            "category": "Machinery Remix",
-            "thumbnail": "images/goat2.jpg",
-        },
-        {
-        "name": "Deer Phil Collins",
-        "source": "2ft954vXPa4",
-        "desc": "Just dropping by for the fill.",
-        "category": "Animal Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "Jump Around! Jump Around!",
-        "source": "p6DrOUb9cnI",
-        "desc": "Fire alarm feat Cypress Hill.",
-        "category": "Machinery Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "This Dog is Toxic",
-        "source": "AxoriYVxK5U",
-        "desc": "Someone pet the damned dog!",
-        "category": "Animal Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "Doggo in A Minor",
-        "source": "St7S3YrxqW0",
-        "desc": "A better voice than you'll ever have",
-        "category": "Animal Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "I Feel Good!",
-        "source": "Z_FKqLqkyPY",
-        "desc": "Spooky",
-        "category": "Person Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "G-pop",
-        "source": "NLkDLy2TYxQ",
-        "desc": "Someone get Coachella on the phone. This is special.",
-        "category": "Animal Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "The most important video on YouTube",
-        "source": "Ns7Z8ag4oSY",
-        "desc": "Heaven let your light shine meow.",
-        "category": "Animal Remix",
-        "thumbnail": "images/goat1.jpg",
-        },
-        {
-        "name": "M.I.A - Paper ****s",
-        "source": "1tLyn1R0eR8",
-        "desc": "I see a very fruitful colab album in the near future.",
-        "category": "People Remix",
-        "thumbnail": "images/goat1.jpg",
-        }
-    ]
-};
+let videoList, categoryList;
 
 function init(){
-    displayVideos(videoList.videos);
-    videoSearchEl.on('keyup', function(evt){
-        displayVideosByTitle($(this).val());
+    //get videos
+    $.getJSON('json/videos.json', function(videos){
+        videoList = videos;
+        displayVideos(videoList.videos);
+        videoSearchEl.on('keyup', function(evt){
+            displayVideosByTitle($(this).val());
+        });
     });
+    //get categories
+    $.getJSON('json/categories.json', function(categories){
+        categoryList = categories;
+        displayCategories(categoryList.categories);
+    });
+    backToVideosBtn.on('click', swapScreens);
 };
 
+
+/**
+ * @param {string} title
+ * Takes search input and filters videos by title
+ */
 function displayVideosByTitle(name){
     let filteredVideos = videoList.videos.filter(function(video){
         return video.name.toLowerCase().includes(name.toLowerCase());
@@ -95,8 +39,13 @@ function displayVideosByTitle(name){
     displayVideos(filteredVideos);
 }
 
+
+/**
+ * @param {object} video
+ * Get the HTML string for one video list item.
+ */
 function getVideoItemHTML(i, video){
-    return `<li data-id="${i}"class="hex">
+    return `<li data-id="${video.id}"class="hex">
                 <div class="hexIn">
                     <a class="hexLink" href="#"> 
                         <img class="thumbnailimg" src="https://i.ytimg.com/vi/${video.source}/hqdefault.jpg">
@@ -107,24 +56,71 @@ function getVideoItemHTML(i, video){
             </li>`;
 }
 
+/**
+ * Display a list of videos.
+ */
 function displayVideos(videos){
     let htmlString ='';
     $.each(videos, function(i, video){
         htmlString = htmlString + getVideoItemHTML(i, video);
-        // thumbHtml += '<li data-id="' + i + '"class="hex"><div class="hexIn"><a class="hexLink" href="#">' + '<img class="thumbnailimg" src=' + item.thumbnail + '><h1>' + item.name + '</h1><p>' + item.desc + '</p></a></div></li>';
     });
     videoThumbsEl.html(htmlString);
+    //add click event listener to each video item
+    let videoItems = $('.hex');
+    videoItems.on('click',  function(){
+        playVideo($(this).data('id'));
+        swapScreens();
+    });
 }
+
+/**
+ * Play the video.
+ * @param {String} videoId 
+ */
+function playVideo(videoId){
+    let youtubeId = videoList.videos[videoId].source;
+    videoPlayerEl.attr("src", 'http://www.youtube.com/embed/' + youtubeId + '?autoplay=1');
+}
+
+function swapScreens(){
+    if (containerEl.hasClass('is-open')) {
+        containerEl.removeClass('is-open');
+    }
+    else {
+        containerEl.addClass('is-open');
+    }
+}
+/**
+ * Get the HTML string for one category list item.
+ * @param {Object} category 
+ */
+function getCategoryItemHTML(category){
+    return `<li class="category-item" data-slug="${category.slug}">
+                ${category.title}
+            </li>`;
+}
+
+/**
+ * Display the video categories
+ * @param {Object} categories 
+ */
+function displayCategories(categories){
+    let htmlString ='';
+    $.each(categories, function(i, category){
+        htmlString = htmlString + getCategoryItemHTML(category);
+    });
+    categoryListEl.html(htmlString);
+}
+
 
 // videoThumbsEl.html(thumbHtml);
 
-videoThumbsEl.on('click', 'li', function(){
-    let index = ($(this).attr("data-id"));
-    // videoPlayerEl.attr("src", videoList[index].source);
-    videoPlayerEl.attr("src", `https://www.youtube.com/embed/${videoList.videos[index].source}`);
-    videoTitleEl.html("<h1>" + videoList.videos[index].name + "</h1>");
-    videoDescEl.html(videoList.videos[index].desc);
-    containerEl.css("left", "4000px");
-});
+// videoThumbsEl.on('click', 'li', function(){
+//     let index = ($(this).attr("data-id"));
+//     videoPlayerEl.attr("src", `https://www.youtube.com/embed/${videoList.videos[index].source}?autoplay=1`);
+//     videoTitleEl.html("<h1>" + videoList.videos[index].name + "</h1>");
+//     videoDescEl.html(videoList.videos[index].desc);
+//     containerEl.css("left", "4000px");
+// });
 
 init();
