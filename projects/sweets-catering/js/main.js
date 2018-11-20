772fb3436ab7eaad6f5c0f11e328a86a59172431
Geoff Sargison <gsarge.inzid@gmail.com>
2018-11-20 18:51:50 +1300

    changed header, added some map stuff. Added other map projects for referenece

--- projects/sweets-catering/js/main.js
+++ projects/sweets-catering/js/main.js
@@ -2,7 +2,7 @@ let sweetsEl = $('#sweets'),
     sweetItemsEl = $('#sweet-items'),
     beltEl = $('.belt'),
     eventNameEl = $('#eventName'),
-    eventDescEl = $('#eventDesc'),
+    dateEl = $('#eventDate');
     noOfAttendeesEl = $('#noOfAttendees'),
     submitFormBtn = $('#submitForm');
     event = {};
@@ -11,20 +11,19 @@ let sweetsEl = $('#sweets'),
     quoteScreen = $('#quote');
     confirmedScreen = $('#confirmed');
     eventTitleEl = $('.eventTitle');
-    confirmEventDescEl = $('.event-description')
     confirmSweetImageEl = $('.confirm-sweet-image');
+    confirmEventDateEl = $('.confirmEventDate')
     confirmOrderMessageEl = $('.confirm-order-message');
     confirmTotalPriceEl = $('.confirm-total-price')
     confirmOrderBtn = $('#confirmOrderBtn');
     cancelOrderBtn = $('#cancelOrderBtn')
 
-    // var popup = L.popup();
 
 let sweetsList,
     categoryList
 
 //leaflet maps
-let mymap = L.map('mapid').setView([-43.491053, 172.57902], 13),
+let map = L.map('mapid').setView([-43.491053, 172.57902], 13),
 jellyIcon = L.icon({
     iconUrl: '../img/jellybeanicon.png',
     shadowUrl: '../img/jellybeanicon-shadow.png',
@@ -44,7 +43,7 @@ personicon = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 })
 
-marker = L.marker([-43.491053, 172.57902], {icon: jellyIcon}).addTo(mymap);
+marker = L.marker([-43.491053, 172.57902], {icon: jellyIcon}).addTo(map);
 
 function init() {
     //get categories
@@ -74,25 +73,57 @@ function init() {
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoic2FyZ2ltbyIsImEiOiJjam9ucHUwdjQweHFqM3FsZTM5NzhjajlsIn0.l9URIGr2w1jZ3pUxuVM_tw'
-    }).addTo(mymap);
-    marker.bindPopup("<b>Sweet as Sweets Emporium</b>").openPopup();
-    mymap.locate({setView: true, maxZoom: 16});
-    mymap.on('locationfound', onLocationFound);
-    mymap.on('locationerror', onLocationError);
+    }).addTo(map);
+    map.on('click', function(e) {
+        let container = L.DomUtil.create('div'),
+            startBtn = createButton('Start from this location', container),
+            destBtn = createButton('Go to this location', container);
+        L.popup()
+            .setContent(container)
+            .setLatLng(e.latlng)
+            .openOn(map);
+    }); 
+    L.Routing.control({
+        waypoints: [
+            L.latLng(-41.491053, 170.57902),
+            L.latLng(-43.491053, 172.57902)
+        ],
+        routeWhileDragging: true
+    }).addTo(map);
+    L.DomEvent.on(startBtn, 'click', function() {
+        control.spliceWaypoints(0, 1, e.latlng);
+        map1.closePopup();
+    });
+    L.DomEvent.on(destBtn, 'click', function() {
+        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+        map1.closePopup();
+    });    
+    // marker.bindPopup("<b>Sweet as Sweets Emporium</b>").openPopup();
+    map.locate({setView: true, maxZoom: 16});
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
+
 }
 
-//leaflet geolocation
+//leaflet functions
 function onLocationFound(e) {
-    var radius = e.accuracy / 2;
+    let radius = e.accuracy / 2;
 
     L.marker(e.latlng, {icon: personicon}).addTo(mymap)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-    L.circle(e.latlng, radius).addTo(mymap);
 }
 
 function onLocationError(e) {
     alert(e.message);
 }
+
+function createButton(label, container) {
+    let btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
+
+
 
 
 /**
@@ -118,11 +149,12 @@ function displaySweets(sweets) {
     });
     //displays sweets and adds back to categories button
     sweetItemsEl.html(htmlString + '<input id="backToCategories" type="button" value="Back to Categories">');
-    let backToCategories = $("#backToCategories");
-    backToCategories.on('click', function(){
+    let backToCategoriesBtn = $("#backToCategories");
+    backToCategoriesBtn.on('click', function(){
         beltEl.toggleClass('slide');
         sweetsEl.toggleClass('slide');
         sweetItemsEl.toggleClass('slide');
+        backToCategoriesBtn.toggleClass('active');
     });
     //adds click listeners to the sweets to make final sweet selection
     let sweetItems = $(".sweet-item");
@@ -151,9 +183,11 @@ function displayCategories(categories) {
         let categoryid = $(this).data('categoryid');
         let filteredSweets = filterByCategory(sweetsList.sweets, categoryid);
         displaySweets(filteredSweets);
+        let backToCategoriesBtn = $("#backToCategories");
         beltEl.toggleClass('slide');
         sweetsEl.toggleClass('slide');
         sweetItemsEl.toggleClass('slide');
+        backToCategoriesBtn.toggleClass('active');
     });
 }
 
@@ -187,10 +221,15 @@ function getQuote(){
     let chosenSweet = sweetsList.sweets[chosenSweetId];
     let noOfAttendees = (parseInt(noOfAttendeesEl.val()));
     let totalPrice = (noOfAttendees * chosenSweet.price);
+    //date controls
+    let dateOfEvent = new Date (dateEl.val());
+    day = dateOfEvent.getDate();
+    month = dateOfEvent.getMonth() + 1;
+    year = dateOfEvent.getFullYear();
     event.title = eventNameEl.val();
+    event.date = `${day}/${month}/${year}`;
     event.totalPrice = totalPrice.toFixed(2);
     event.chosenSweet = chosenSweet.title;
-    event.desc = eventDescEl.val();
     event.chosenSweetPrice = chosenSweet.price.toFixed(2);
     event.attendees = noOfAttendees;
     event.chosenSweetImage = chosenSweet.image;
@@ -201,9 +240,9 @@ function getQuote(){
  */
 function loadQuoteScreen(){
     eventTitleEl.html(event.title);
-    confirmEventDescEl.html(event.desc);
+    confirmEventDateEl.html(event.date)
     confirmSweetImageEl.html(`<img src="img/${event.chosenSweetImage}.jpg">`);
-    confirmOrderMessageEl.html(`${event.chosenSweet}'s provided for ${event.attendees} attendees at $${event.chosenSweetPrice} each.`)
+    confirmOrderMessageEl.html(`<span class="pinkText">${event.chosenSweet}'s</span> provided for <span class="pinkText">${event.attendees}</span> attendees at <span class="pinkText">$${event.chosenSweetPrice}</span> each.`)
     confirmTotalPriceEl.html(`Total: $${event.totalPrice}`);
 }
 
